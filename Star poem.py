8b0ec7ded6550f3343582d64fe5a0d3186bf3ab0
SyedaHiraAmjad <ha458170@gmail.com>
2021-05-10 00:30:17 +0500

    Add files via upload

---
+++ Star poem.py
@@ -0,0 +1,8 @@
+'''Twinkle, twinkle, little star,
+	How I wonder what you are! 
+		Up above the world so high,   		
+		Like a diamond in the sky. 
+Twinkle, twinkle, little star, 
+	How I wonder what you are'''
+#We'll try this one 
+print("Twinkle, twinkle, little star, \nHow I wonder what you are! \n\t Up above the world so high, \n\t Like a diamond in the sky. \n Twinkle, twinkle, little star, \n\t How I wonder what you are.")
